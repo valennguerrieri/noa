89fa41bb78c5d4ed0b0dd96333f63c0a2facaeb8
valenng <valenguerrieri@gmail.com>
2026-09-07 18:07:38 -0300

    fix: dejar de rastrear archivos de environment del frontend

--- noa-frontend/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    isElectron: true,
-    production: true,
-    geminiApiKey: 'AIzaSyCGsQOA8JBk3yVukaB7c7fxrGfWNg7ATmA',
-    supabaseUrl: 'https://dxuzaeicabliqftqlfmm.supabase.co',
-    supabaseKey: 'sb_publishable_QPW9vXO__tIQnUsw4KWtAg_NpjvLvXv'
-};
